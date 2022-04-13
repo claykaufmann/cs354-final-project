6403d02b8aafc5604de514ade29161c453029383
Clay Kaufmann <claykaufmann@gmail.com>
2022-04-12 20:14:28 -0400

    work on load data

--- data_processing/load_dataset.py
+++ data_processing/load_dataset.py
@@ -4,13 +4,33 @@ this file loads the data set for pytorch
 import torch
 import torch.nn as nn
 import torch.utils.data as data
+import math
 
 
-class VideoGameMusicDataset(data.IterableDataset):
-    def __init__(self, start, end) -> None:
+class VideoGameMusicDataset(data.Dataset):
+    """
+    represents the video game music dataset
+    """
+
+    def __init__(self, data_folder_path) -> None:
         super(VideoGameMusicDataset).__init__()
 
-        assert end > start
+        self.root_dir = data_folder_path
 
-        self.start = start
-        self.end = end
+        self.files = self.get_files()
+
+    def get_files(self):
+        """
+        return a list of files within the data folder
+        """
+        files = []
+
+        return files
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        """
+        get actual items
+        """
