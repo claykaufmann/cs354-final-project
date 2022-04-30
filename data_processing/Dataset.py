fcc4ec65c84e28753a6e49d0988388dad17df9e7
Clay Kaufmann <claykaufmann@gmail.com>
2022-04-30 18:49:27 -0400

    add maestro dataset to dataset

--- data_processing/Dataset.py
+++ data_processing/Dataset.py
@@ -7,9 +7,31 @@ import numpy as np
 import os
 from util.constants import *
 import random
+import muspy
 
 
 SEQUENCE_START = 0
+
+
+def get_maestro_dataset(path: str, representation: str = "pianoroll"):
+    """
+    a simple wrapper for collecting maestro dataset
+
+    params:
+    path: path to store the data
+    representation: the way to represent the dataset
+    """
+    maestro = muspy.MAESTRODatasetV3(path, download_and_extract=True)
+
+    maestro.convert()
+
+    data = maestro.to_pytorch_dataset(representation=representation)
+
+    train_size = int(0.85 * len(data))
+    test_size = len(data) - train_size
+    train_data, test_data = torch.utils.data.random_split(data, [train_size, test_size])
+
+    return train_data, test_data
 
 
 class VideoGameMusicDataset(data.Dataset):
